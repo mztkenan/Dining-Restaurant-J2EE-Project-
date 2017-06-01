90da00b3efb75d65ddf200333d2e7f43044e1ec1
BeyondWang <13391790751@163.com>
2017-06-01 19:58:02 +0800

    delete方法已经实现

--- src/com/cugb/javaee/action/DeleteItems.java
+++ src/com/cugb/javaee/action/DeleteItems.java
@@ -1,18 +1,31 @@
 package com.cugb.javaee.action;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.cugb.javaee.bean.CartItem;
 
 public class DeleteItems extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+		HttpSession session = request.getSession(false);
+		ArrayList<CartItem> cartItemArray=(ArrayList<CartItem>) session.getAttribute("carItemArray");
+		String PID=request.getParameter("PID");
+		for(int i= 0 ; i<cartItemArray.size() ; i++){
+			if(cartItemArray.get(i).getDish().getProductId().equals(PID)){
+				cartItemArray.remove(i);
+				break;
+			}
+		}
+		session.setAttribute("carItemArray", cartItemArray);
 	}
 
 	@Override
